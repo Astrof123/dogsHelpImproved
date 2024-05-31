79d35401ef40a0de01264e4389f0a6383b93c31e
Astrof123 <71438030+Astrof123@users.noreply.github.com>
2024-05-31 19:23:46 +0800

    Add files via upload

--- script_client.py
+++ script_client.py
@@ -1,48 +1,74 @@
 import requests
 import json
 from time import sleep
+import random
 
 def requesterReg():
-    uri = 'http://127.0.0.1:8000/dogs/register'
+    uri = 'http://127.0.0.1:8001/dogs/register'
     adminToken = input('Введите токен админа: ')
     charact = input('Введите характеристику собаки: ')
     place = input('Введите местоположение собаки: ')
+    photo = input('Введите характеристику собаки: ')
+    name = input('Введите местоположение собаки: ')
 
-    try:
-        response = requests.post(uri, data={
+    dates = json.dumps({
             'accessToken': adminToken,
             'characteristic': charact,
-            "place": place
+            'place': place,
+            'photo': photo,
+            'name': name
         })
-        if (response.status_code == 200):
-            jsn = json.loads(response.text)
-            return [jsn["success"], jsn['dog_id'], jsn['accessDogToken']]
-        else:
-            print('Ошибка')
-
-    except Exception as exception:
-        print(exception)
-
-def requesterUpdate(accessDogToken, dog_id):
-    uri = 'http://127.0.0.1:8000/dogs/update'
 
     try:
-        response = requests.post(uri, data={
-            'accessDogToken': accessDogToken,
-            'dog_id': dog_id,
-            "coordinates": coordinates
-        })
+        response = requests.post(uri, data = dates)
+        print(response)
         if (response.status_code == 200):
             jsn = json.loads(response.text)
-            return jsn["success"]
+            return [jsn["success"], jsn['dogid'], jsn['accessDogToken']]
         else:
-            return [False, False, False]
+            print('Ошибка')
 
     except Exception as exception:
         print(exception)
         exit()
 
 
+def requesterUpdate(accessDogToken, dog_id, coordinates):
+    uri = 'http://127.0.0.1:8001/dogs/update'
+
+    try:
+        dates = json.dumps({
+            'accessDogToken': accessDogToken,
+            'dogid': dog_id,
+            "coordinates": coordinates
+        })
+
+        response = requests.post(uri, data=dates)
+        if (response.status_code == 200):
+            jsn = json.loads(response.text)
+            return jsn["success"]
+
+    except Exception as exception:
+        print(exception)
+        exit()
+
+
+flag = False
+while not(flag):
+    flag, dog_id, accessDogToken = requesterReg()
+
+coordinatesW = 52.250323
+coordinatesD = 143.413123
+
+while True:
+    coords = str(coordinatesW) + ', ' + str(coordinatesD)
+
+    requesterUpdate(accessDogToken, dog_id, coords)
+
+    coordinatesW += random.randint(-5, 5)
+    coordinatesD += random.randint(-5, 5)
+
+    sleep(60)
 
 flag = False
 while not(flag):
@@ -52,8 +78,10 @@ coordinatesW = '52.250323'
 coordinatesD = '143.413123'
 
 while True:
-    requesterUpdate(accessDogToken, dog_id)
+    coordinates = coordinatesW + coordinatesD
+    requesterUpdate(accessDogToken, dog_id, coordinates)
 
-    coordinates = ''
+    coordinatesW = str(int(coordinatesW) + 1)
+    coordinatesD = str(int(coordinatesD) + 1)
 
     sleep(60)
